2bc2286db3c661fd3650d4dd67bb75f78bda7ac6
unknown <nagai_kei@hotmail.com>
2018-04-04 19:57:43 -0500

    Se añadieron muchas funcionalidades incluyendo servicios y nuevas ventanas y en fin un total rework

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
+//Servicios
+import { ListaDeseosService } from './services/lista-deseos.services';
+
+//Pipes
+import { PlaceHolderPipe } from './pipes/placeholder.pipe';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -10,13 +15,17 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { TabsPage } from '../pages/tabs/tabs';
 import { PendientesComponent } from '../pages/pendientes/pendientes.component';
 import { TerminadosComponent } from '../pages/terminados/terminados.component';
+import { AgregarComponent } from '../pages/agregar/agregar.component';
+
 
 
 @NgModule({
   declarations: [
     MyApp,
+    PlaceHolderPipe,
     PendientesComponent,
     TerminadosComponent,
+    AgregarComponent,
     TabsPage
   ],
   imports: [
@@ -28,9 +37,11 @@ import { TerminadosComponent } from '../pages/terminados/terminados.component';
     MyApp,
     PendientesComponent,
     TerminadosComponent,
+    AgregarComponent,
     TabsPage
   ],
   providers: [
+    ListaDeseosService,
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
